7bf69a3c0576299fca0d8543830c234f9febc39f
Arman Idrisi <119788224+Armanidrisi@users.noreply.github.com>
2023-06-01 00:48:17 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,6 +5,12 @@ const PORT = process.env.PORT || 3000;
 
 app.use("/api/quotes",require('./routes/quotes'));
 
+app.get("*",(req,res)=>{
+res.status(400).json({
+docs:"https://armanidrisi.github.io/quote-generator-api/"
+})
+});
+
 app.listen(PORT, () => {
   console.log(`Listening On port ${PORT}`);
 });
